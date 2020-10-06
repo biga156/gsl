ac5f600c22c6f4edfdcf91e5d7668681db51d388
biga156 <biga156@gmail.com>
2020-10-06 17:24:55 +0200

    first commit

---
+++ sites/default/files/php/twig/5f7c6c7e9b9fa_input--date.html.twig_U3fHJUNsF8jKq8YVoP7FzKPrp/4r2cLqii3Y3uHSHJSC-UTmHVFwdZXtckZa4aNOhVmPk.php
@@ -0,0 +1,92 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/d8w3css/templates/form/input--date.html.twig */
+class __TwigTemplate_35c983b12f30133d44a277d5c43d84be5a7670372e4cbe6912f06f3c2aa56794 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 14];
+        $filters = ["escape" => 21];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 14
+        $context["classes"] = [0 => "w3-half", 1 => "w3-input", 2 => "w3-border", 3 => "w3-theme-border"];
+        // line 21
+        echo "<input";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo " />";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/d8w3css/templates/form/input--date.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  57 => 21,  55 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/d8w3css/templates/form/input--date.html.twig", "C:\\wamp64\\www\\drupal_gsl\\themes\\d8w3css\\templates\\form\\input--date.html.twig");
+    }
+}
